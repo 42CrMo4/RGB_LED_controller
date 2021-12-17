f734cbbcab31a57858440296b651cefca19dc9cf
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2021-12-17 15:10:25 +0100

    SW: formating

--- RGB_LED_controller.ino
+++ RGB_LED_controller.ino
@@ -1,24 +1,28 @@
-int led_blue = PIN_PA4;           // the PWM pin the LED is attached to
-int led_green = PIN_PA5;           // the PWM pin the LED is attached to
-int led_red = PIN_PB2;           // the PWM pin the LED is attached to
-int brightness_blue = 240;    // how bright the LED is
-int brightness_green = 120;    // how bright the LED is
-int brightness_red = 0;    // how bright the LED is
-int fadeAmount_blue = 1;    // how many points to fade the LED by
-int fadeAmount_green = 1;    // how many points to fade the LED by
-int fadeAmount_red = 1;    // how many points to fade the LED by
+/*
+    Basic RGB Fade example - based on https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
+*/
 
-// the setup routine runs once when you press reset:
+int led_blue = PIN_PA4;           // the PWM pin the blue LED is attached to
+int led_green = PIN_PA5;          // the PWM pin the green LED is attached to
+int led_red = PIN_PB2;            // the PWM pin the red LED is attached to
+
+int brightness_blue = 240;        // how bright the blue LED is
+int brightness_green = 120;       // how bright the green LED is
+int brightness_red = 0;           // how bright the red LED is
+
+int fadeAmount_blue = 1;          // how many points to fade the blue LED by
+int fadeAmount_green = 1;         // how many points to fade the green LED by
+int fadeAmount_red = 1;           // how many points to fade the red LED by
+
+// setup all LED Pins as output
 void setup() {
-  // declare pin 9 to be an output:
   pinMode(led_blue, OUTPUT);
   pinMode(led_green, OUTPUT);
   pinMode(led_red, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of all LEDs
   analogWrite(led_blue, brightness_blue);
   analogWrite(led_green, brightness_green);
   analogWrite(led_red, brightness_red);
@@ -38,6 +42,7 @@ void loop() {
   if (brightness_red <= 0 || brightness_red >= 255) {
     fadeAmount_red = -fadeAmount_red;
   }
-  // wait for 30 milliseconds to see the dimming effect
+
+  // wait for 25 milliseconds to see the dimming effect
   delay(25);
 }
